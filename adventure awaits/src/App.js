1e2dcc5f014ccef46b819318a8567d4299ca7285
MuchiriAndrew <kariukia225@gmail.com>
2023-05-03 16:33:10 +0300

    footer complete

--- adventure awaits/src/App.js
+++ adventure awaits/src/App.js
@@ -3,6 +3,10 @@ import Navbar from './components/Navbar'
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css'
 import Home from './components/pages/Home'
+import SignUp from './components/pages/SignUp';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+
 
 function App() {
   return(
@@ -10,8 +14,10 @@ function App() {
     <Router>
       <Navbar />
         <Switch>
-          <Route path = '/' exact component =
-           {Home} />
+          <Route path = '/' exact component ={Home} />
+           <Route path = '/services' component ={Services} />
+           <Route path = '/sign-up' component ={SignUp} />
+           <Route path = '/products' component ={Products} />
         </Switch>
     </Router>
    </>
--- adventure awaits/src/components/HeroSection.js
+++ adventure awaits/src/components/HeroSection.js
@@ -7,9 +7,9 @@ function HeroSection() {
   return (
     <div className='hero-container'>
 
-         <video src="\videos\video-2.mp4"autoPlay loop muted/>
+         <video src="\videos\video-1.mp4"autoPlay loop muted/>
         <h1>ADVENTURE AWAITS</h1>
-        <p>What Are You Waiting For</p>
+        <p>What Are You Waiting For ?</p>
         <div className='hero-btns'>
 
             <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
