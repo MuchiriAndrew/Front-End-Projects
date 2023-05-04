0fbb807c8397c10793c54d0c2002ca34a46badc3
MuchiriAndrew <kariukia225@gmail.com>
2023-05-04 11:13:15 +0300

    update

--- loopstudios landing page/src/components/Header.js
+++ loopstudios landing page/src/components/Header.js
@@ -3,14 +3,13 @@ import  Navbar  from './Navbar'
 
 function Header() {
   return (
-    <>
-    
+
+    <>    
     <Navbar />
     
     <div>
         IMMERSIVE EXPERIENCES THAT DELIVER
     </div>
-    
     </>
     
   )
--- loopstudios landing page/src/components/Navbar.js
+++ loopstudios landing page/src/components/Navbar.js
@@ -1,9 +1,90 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import {Link} from 'react-router-dom'
 
-function Navbar() {
+
+  function Navbar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+  
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+  
+    const showButton = () => {
+      if(window.innerWidth <= 960) {
+        setButton(false);
+      }else{
+        setButton(true);
+      }
+    };
+  
+    useEffect(() => {
+      showButton()
+    },[])
+    //remember when you leave the second array parameter empty i.e [] the useEffect hook will run on mount only.
+  
+  
+  
+    window.addEventListener('resize', showButton)
+
   return (
-    <div>Navbar</div>
+
+    <>
+
+    <nav className='navbar'>
+
+
+        <div className='navbar-container'>
+
+           <Link to = "/" className = "navbar-logo" onClick = {closeMobileMenu} >
+           <img src="../../images/logo.svg" alt="loopstudios" />
+           </Link>
+           {/* <link> replaces the <a> tag */}
+
+           <div className='menu-icon' onClick={handleClick}>
+            <i className = {click ? 'fas fa-times' : 'fas fa-bars'} />
+           </div>
+
+           <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+
+            <li className='nav-item'>
+            <Link to='/about' className='nav-links' onClick= {closeMobileMenu}>
+              About
+            </Link>
+            </li>
+
+            <li className='nav-item'>
+            <Link to='/careers' className='nav-links' onClick= {closeMobileMenu}>
+              Careers
+            </Link>
+            </li>
+
+            <li className='nav-item'>
+            <Link to='/events' className='nav-links' onClick= {closeMobileMenu}>
+              Events
+            </Link>
+            </li>
+
+            <li className='nav-item'>
+            <Link to='/products' className='nav-links-mobile' onClick= {closeMobileMenu}>
+              Products
+            </Link>
+            </li>
+
+            <li className='nav-item'>
+            <Link to='/support' className='nav-links' onClick= {closeMobileMenu}>
+              Support
+            </Link>
+            </li>
+
+           </ul>
+
+           {/* {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>} */}
+        </div>
+    </nav>
+
+    </>
   )
 }
+
 
 export default Navbar
