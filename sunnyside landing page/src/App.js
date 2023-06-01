96d30f6b7ffe6313917b1d5b01fc9407e6096706
MuchiriAndrew <kariukia225@gmail.com>
2023-06-01 20:22:46 +0300

    update

--- sunnyside landing page/src/App.js
+++ sunnyside landing page/src/App.js
@@ -1,11 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
+// import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import './App.css'
+import Home from './components/pages/Home'
+// import About from './components/pages/About';
+// import Contact from './components/pages/Contact';
+// import Services from './components/pages/Services';
+
 
 function App() {
   return(
-    <>
-    
-    </>
-
+   <>
+   <Home />
+    {/* <Router>
+        <Routes>
+           <Route path = '/' exact component ={Home} />
+           <Route path = '/about' component ={About} />
+           <Route path = '/contact' component ={Contact} />
+           <Route path = '/services' component ={Services} />
+        </Routes>
+    </Router> */}
+   </>
   );
 }
 
---
+++ sunnyside landing page/src/components/pages/Home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+// import '../../App.css'
+import MyNavbar from '../Navbar'
+// import Header from '../Header'
+// import HeroSection from '../Herosection'
+// import Creations from '../Creations';
+// import Footer from '../Footer';
+
+function Home() {
+    return(
+        <>
+        <MyNavbar />
+        {/* <offCanvas />
+        <Header />
+        <Body/>
+        <Testimonials />
+        <Images />
+        <Footer /> */}
+        </>
+
+    )
+}
+
+export default Home;
