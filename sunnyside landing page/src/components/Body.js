960105908ae2e7b8433654f5603ef88fac78d257
MuchiriAndrew <kariukia225@gmail.com>
2023-06-03 00:20:45 +0300

    update

--- sunnyside landing page/src/components/Body.js
+++ sunnyside landing page/src/components/Body.js
@@ -11,21 +11,38 @@ function Body() {
 
 <Container fluid>
 
-      <Row className='bg-danger'>
-        <Col md={6}>TRANSFORM YOUR BRAND</Col>
-        <Col className='d-md-none'><img src="../../images/mobile/image-transform.jpg" style={{width:"100%"}} alt="egg" /></Col>
-        <Col className='d-none d-md-block text-start g-0' md={6}><img src="../../images/desktop/image-transform.jpg" style={{width:"100%"}} alt="egg" /></Col>
-      </Row>
-
-      <Row className='bg-primary'>
-      <Col className='d-md-none'><img src="../../images/mobile/image-transform.jpg" style={{width:"100%"}} alt="egg" /></Col>
-        <Col className='d-none d-md-block g-0' md={6}><img  src="../../images/desktop/image-transform.jpg" style={{width:"100%"}} alt="egg" /></Col>
-        <Col md={6}>STAND OUT TO THE RIGHT AUDIENCE</Col>
+      <Row>
+        <Col md={6}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat odio harum iste quia, placeat consequuntur? Numquam ipsa aperiam, repellat reprehenderit debitis totam reiciendis eveniet facilis nisi consectetur. Placeat, in officia.</Col>
+        
+        <Col className='gx-0' md={6}>
+            <img className='img-fluid d-md-none' src="../../images/mobile/image-transform.jpg" style={{width:"100%"}} alt="egg" />
+            <img className='img-fluid d-none d-md-block' src="../../images/desktop/image-transform.jpg" style={{width:"100%"}} alt="egg" />
+        </Col>
       </Row>
 
       <Row>
-        <Col md={6}><img className='img-fluid' src="" alt="" /></Col>
-        <Col md={6}><img className='img-fluid' src="" alt="" /></Col>
+
+      <Col className='gx-0' md={6}>
+            <img className='img-fluid d-md-none' src="../../images/mobile/image-stand-out.jpg" style={{width:"100%"}} alt="egg" />
+            <img className='img-fluid d-none d-md-block' src="../../images/desktop/image-stand-out.jpg" style={{width:"100%"}} alt="egg" />
+        </Col>
+
+        <Col md={6}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat distinctio sunt ut tenetur deleniti temporibus suscipit vero ad, esse dolore aperiam incidunt quis, sapiente nostrum dolorum possimus, eligendi adipisci quas?</Col>
+      </Row>
+
+      <Row>
+        <Col id='col1' className='gx-0' md={6}>
+            <img className='img-fluid d-md-none' src="../../images/mobile/image-graphic-design.jpg" style={{width:"100%"}} alt="egg" />
+            <img className='img-fluid d-none d-md-block' src="../../images/desktop/image-graphic-design.jpg" style={{width:"100%"}} alt="egg" />
+            <div className='overlayText1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam molestiae voluptas sint quibusdam, omnis accusantium totam mollitia sequi optio laborum fugiat distinctio temporibus? Reprehenderit accusantium numquam porro corrupti expedita tenetur!</div>
+        </Col>
+        
+        <Col id='col2'className='gx-0' md={6}>
+            <img className='img-fluid d-md-none' src="../../images/mobile/image-photography.jpg" style={{width:"100%"}} alt="egg" />
+            <img className='img-fluid d-none d-md-block' src="../../images/desktop/image-photography.jpg" style={{width:"100%"}} alt="egg" />
+            <div className='overlayText1'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum autem quis nam rem, eveniet dicta fuga officia repudiandae sed porro voluptatibus, dolorem cupiditate iusto magni hic dolorum non, maiores quae.</div>
+        </Col>
+       
       </Row>
 
 </Container>
