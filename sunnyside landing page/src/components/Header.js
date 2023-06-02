f69824dcc5a3ace5a49a2c35fe11b7832fc675a2
MuchiriAndrew <kariukia225@gmail.com>
2023-06-02 16:51:11 +0300

    update

--- sunnyside landing page/src/components/Header.js
+++ sunnyside landing page/src/components/Header.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import NavbarComp from './NavbarComp'
+import './Header.css';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Header() {
   return (
     <>
+    <div className='bg-image'>
+    <NavbarComp />
     
+    {/* <div id='wrapper' className='container' >
+    <h1>WE ARE CREATIVES</h1>
+    <img src="../../images/icon-arrow-down.svg" alt="down arrow" />
+    </div> */}
+
+    <Container>
+      <Row id='row1' className="text-center text-white justify-content-center align-items-center">
+        <Col  xs={6}>
+        <h1 style={{fontFamily:"Frances", fontWeight:"900",}}>WE ARE CREATIVES</h1>
+        </Col>
+      </Row>
+
+      <Row id='row2' className="text-center justify-content-center align-items-center">
+      <Col  xs={6}> <img  src="../../images/icon-arrow-down.svg" alt="down arrow" /></Col>
+      </Row>
+    </Container>
+   
+
+    </div>
+
     </>
   )
 }
---
+++ sunnyside landing page/src/components/NavbarComp.js
@@ -0,0 +1,43 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import './NavbarComp.css';
+
+function NavbarComp() {
+  return (
+    <>
+      {['md'].map((expand) => (
+        <Navbar key={expand} bg="transparent" expand={expand}>
+          <Container fluid >
+            <Navbar.Brand className='ms-3' href="#"><img src="../../images/logo.svg" alt="sunnyside" /></Navbar.Brand>
+
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas id={`offcanvasNavbar-expand-${expand}`} aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`} placement="end" >
+
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Offcanvas
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+
+                  <Nav.Link className='text-light me-4' href="#action1">About</Nav.Link>
+                  <Nav.Link className='text-light me-4' href="#action2">Services</Nav.Link>
+                  <Nav.Link className='text-light me-4' href="#action2">Projects</Nav.Link>
+                  <Nav.Link id='contact-link' className='text-black me-4 ' href="#action2">CONTACT</Nav.Link>
+                  
+                </Nav>
+               
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
+  );
+}
+
+export default NavbarComp;
