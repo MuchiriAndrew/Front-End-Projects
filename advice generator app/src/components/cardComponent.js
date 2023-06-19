740e48e48501cf0f5928a2331a69553dd3326655
MuchiriAndrew <kariukia225@gmail.com>
2023-06-19 14:43:21 +0300

    update

--- advice generator app/src/components/cardComponent.js
+++ advice generator app/src/components/cardComponent.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import "./cardComponent.css"
+import axios from 'axios';
 
 function CardComponent() {
+
+    const [advice, setAdvice] = useState("");
+
+    const getAdvice = async() => {
+        const response = await axios.get("https://api.adviceslip.com/advice")
+        const advice = await response.data.slip;
+        setAdvice(advice);
+    }
+
+   useEffect(()=> {
+        getAdvice();
+   },[])
+
+
   return (
     <Container fluid id='wrapper'>
     <Row id='row'>
@@ -13,12 +28,12 @@ function CardComponent() {
       <Col xs={11} sm={10} md={9} lg={8} xl={6} className='content rounded-3'>
 
         
-        <p className='mt-4' style={{color:" hsl(150, 100%, 66%)"}}>ADVICE #1</p>
-        <h6 className='mb-4' style={{color:"hsl(193, 38%, 86%)", fontSize:"28px", fontFamily:"Manrope", width:"80%"}}>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui tempore sit, doloremque iste saepe vero nam. Quasi veritatis aperiam earum cum culpa omnis animi qui at soluta? Architecto, error possimus."</h6>
+        <p className='mt-4' style={{color:" hsl(150, 100%, 66%)"}}>ADVICE #{advice.id}</p>
+        <h6 className='mb-4' style={{color:"hsl(193, 38%, 86%)", fontSize:"28px", fontFamily:"Manrope", width:"80%"}}>"{advice.advice}"</h6>
         <img className='img-fluid mb-5 d-none d-lg-block' src="../../images/pattern-divider-desktop.svg" alt="pattern-divider" />
         <img className='img-fluid mb-5 d-block d-lg-none' src="../../images/pattern-divider-mobile.svg" alt="pattern-divider" />
 
-        <div id='circle' className='rounded-circle'>
+        <div onClick={getAdvice} id='circle' className='rounded-circle'>
             <img className='img-fluid' src="../../images/icon-dice.svg" alt="dice" />
         </div>
        
