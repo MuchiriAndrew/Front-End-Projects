476277216425394780834c50f69efd8fcd3ef703
MuchiriAndrew <kariukia225@gmail.com>
2023-06-21 14:39:55 +0300

    update

--- shortly URL shortener landing page/src/components/Statistics.js
+++ shortly URL shortener landing page/src/components/Statistics.js
@@ -10,41 +10,83 @@ function Statistics() {
 
         <div id='surround'>
 
-            <div className='txtwrap '>
-                <h1 id='stath1'>Advanced Statistics</h1>
-                <p id='statp'>Track how your links are perfoming across the web with our advanced statistics dashboard</p>
-            </div>
+            <div id='inner_wrapper'>
 
-
-            <Row className='cardrow'>
-
-                <hr className='backhr p-0'/>
-                <Col id='col_1' md={4} className='bg-white rounded-2 p-0'>
-                    <div className='box'>
-                        <h2 className='cardh2'>Brand Recognition</h2>
-                        <p className='cardp'>Boost your brand recognition with each click. Generic links dont mean a thing. Branded links help instill confidence in your content.</p>
-
+                    <div className='txtwrap  '>
+                        <h1 id='stath1'>Advanced Statistics</h1>
+                        <p id='statp'>Track how your links are perfoming across the web with our advanced statistics dashboard</p>
                     </div>
-                </Col>
-
-                <Col id='col_2' md={4} className='bg-white rounded-2 p-0'>
-                    <div className='box'>
-                            <h2 className='cardh2'>Detailed Records</h2>
-                            <p className='cardp'>Gain insight into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.</p>
-                    </div>
-                </Col>
-
-                <Col md={4} id='col_3' className='bg-white rounded-2 p-0'>
-                    <div className='box'>
-                        <h2 className='cardh2'>Fully Customizable</h2>
-                        <p className='cardp'>Improve brand awareness and content discoverability through customizable links, superchanging audience engagement</p>
-
-                    </div>
-                </Col>
-            </Row>
 
 
+                    <Row className='cardrow'>
 
+                    <hr className='d-none d-md-block backhr p-0'/>
+
+
+                        <Col id='col_1' md={4} className='p-0 gx-3'>
+
+                            <div className='box bg-white rounded-2 '>
+
+                                <div id='inner-box' className=''>
+
+                                <div className='top-circle'>
+                                <img className='img-fluid i' src="../../images/icon-brand-recognition.svg" alt="" />
+                            </div>
+
+                            <div>
+                            <h2 className='cardh2'>Brand Recognition</h2>
+                                <p className='cardp'>Boost your brand recognition with each click. Generic links dont mean a thing. Branded links help instill confidence in your content.</p>
+                            </div>
+
+                                </div>
+
+                            
+
+                            </div>
+                        </Col>
+
+                        
+                        <Col id='col_2' md={4} className='p-0'>
+                            <div className='box bg-white rounded-2'>
+
+                                <div  id='inner-box' className=''>
+
+                                     <div className='top-circle'>
+                                        <img className='img-fluid i' src="../../images/icon-detailed-records.svg" alt="" />
+                                    </div>
+
+                                    <div>
+                                    <h2 className='cardh2'>Detailed Records</h2>
+                                    <p className='cardp'>Gain insight into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.</p>
+
+                                    </div>
+
+                                </div>                                    
+                            </div>
+                        </Col>
+
+
+                        <Col md={4} id='col_3' className='p-0'>
+                            <div className='box bg-white rounded-2'>
+
+                                <div id='inner-box' className=''>
+
+                                <div className='top-circle'>
+                                    <img className='img-fluid i' src="../../images/icon-fully-customizable.svg" alt="" />
+                                </div>
+
+                                <div>
+                                <h2 className='cardh2'>Fully Customizable</h2>
+                                <p className='cardp'>Improve brand awareness and content discoverability through customizable links, superchanging audience engagement</p>
+
+                                </div>
+
+                                </div>                           
+                            </div>
+                        </Col>
+                    </Row>
+
+            </div>   
         </div>
        
     </Container>
