df643e1b4205b7de3ab06037601df2317b0146b9
MuchiriAndrew <kariukia225@gmail.com>
2023-06-21 15:29:51 +0300

    update

--- shortly URL shortener landing page/src/components/Footer.js
+++ shortly URL shortener landing page/src/components/Footer.js
@@ -7,7 +7,21 @@ import Col from 'react-bootstrap/Col';
 
 function Footer() {
   return (
-    <Container fluid className='wrapper5'>
+    <Container fluid className='wrapper5 p-0'>
+
+        <Row className="foot-row bg-primary">
+
+            <Col md={4} className='brand-col bg-warning'>
+                <img id='brandimg' className='img-fluid' src="../../images/logo.svg" alt="logo" />
+            </Col>
+
+            <Col  md={4} className='dets-col bg-danger'>
+            </Col>
+
+            <Col  md={4} className='socials-col bg-success'>
+            </Col>
+
+        </Row>
 
     </Container>
   )
--- shortly URL shortener landing page/src/components/pages/Home.js
+++ shortly URL shortener landing page/src/components/pages/Home.js
@@ -16,7 +16,6 @@ function Home() {
     <Statistics />
     <Boost />
     <Footer />
-    
     </>
     
   )
