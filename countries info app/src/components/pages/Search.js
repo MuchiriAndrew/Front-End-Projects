2396b97c14a8e6002fde3ba1ecf0fc7a99b50cff
MuchiriAndrew <kariukia225@gmail.com>
2023-06-27 16:59:36 +0300

    update

--- countries info app/src/components/pages/Search.js
+++ countries info app/src/components/pages/Search.js
@@ -1,61 +1,210 @@
-import React from 'react'
-import NavbarComp from '../NavbarComp'
+import React, { useState } from 'react'
 import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './Search.css';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Card from 'react-bootstrap/Card';
+import Navbar from 'react-bootstrap/Navbar';
+
 
 function Search() {
+
+  const [mode, setMode] = useState(true);
+  const [moon, setMoon] = useState("bi bi-moon me-2");
+  const [search, setSearch] = useState("bi bi-search")
+
+
+  const handleMode = ()=> {
+    setMode(!mode);
+
+    if(moon === "bi bi-moon me-2" ){
+      setMoon("bi bi-moon-fill text-light me-2")
+    } else {
+      setMoon("bi bi-moon me-2")
+    }
+
+    if(search === "bi bi-search"){
+      setSearch("bi bi-search text-light")
+    } else {
+      setSearch("bi bi-search")
+    }
+  } 
+  
+
+
   return (
     <>
-    <NavbarComp/>
+     <Navbar id='nav' style={{backgroundColor:mode ? "hsl(0, 0%, 100%)" : "hsl(209, 23%, 22%)", height:"10vh"}}>
+      <Container>
+        <h4 style={{color: mode ? "black" : "white"}}>Where in the world?</h4>
 
-    <Container fluid id='wrapper-2' className='p-0'>
+        <div style={{color: mode ? "black" : "white"}}  id='mode-selector' onClick={handleMode}>
+        <i className={moon}></i>
+          Dark Mode
+        </div>
+
+      </Container>
+    </Navbar>
+
+    <Container fluid id='wrapper-2' className='p-0' style={{backgroundColor: mode ? "hsl(0, 0%, 100%)" : "hsl(207, 26%, 17%)"}}>
 
       <div id='search-div' className=''>
 
       <form>
 
-        <button id='searchbtn'><i className="bi bi-search"></i></button>
+        <button id='searchbtn' style={{backgroundColor: mode ? "white" : "hsl(209, 23%, 22%)"}}><i className={search}></i></button>
         <input 
+          style={{backgroundColor: mode ? "white" : "hsl(209, 23%, 22%)"}}
           id='searchbar'
           type="text"
           placeholder='Search for a country'
-          //  <i class="bi bi-search"></i>
         />
       </form>
 
-      <select id='dropdown' >
-            <option >Filter by Region</option>
-            <option value="africa">Africa</option>
-            <option value="america">America</option>
-            <option value="asia">Asia</option>
-            <option value="europe">Europe</option>
-            <option value="oceania">Oceania</option>
-      </select>
 
+      <NavDropdown title="Filter by Region" id="dropdown" style={{backgroundColor: mode ? "white" : "hsl(209, 23%, 22%)", color: mode ? "hsl(200, 15%, 8%)" : "white"}}>
+        <NavDropdown.Item >Africa</NavDropdown.Item>
+        <NavDropdown.Item >America</NavDropdown.Item>
+        <NavDropdown.Item >Asia</NavDropdown.Item>
+        <NavDropdown.Item >Europe</NavDropdown.Item>
+        <NavDropdown.Item >Oceania</NavDropdown.Item>
+      </NavDropdown>
 
-      {/* <div id='dropdown-head'>
-        <div id='txt'>
-        <p className='m-0'>Filter by region</p>
-        <i className="bi bi-chevron-down"></i>
-        </div>
-      </div>
-
-      <div id='dropdown-body'>
-        <ul>
-          <li>Africa</li>
-          <li>America</li>
-          <li>Asia</li>
-          <li>Europe</li>
-          <li>Oceania</li>
-        </ul>
-      </div> */}
-     
       </div>
 
     </Container>
 
-    
-    
+    <Container fluid id='wrapper-3' className='' style={{backgroundColor: mode ? "hsl(0, 0%, 100%)" : "hsl(207, 26%, 17%)"}}>
+
+      <Row className='myrow1  mb-xl-5'>
+
+      <Col id='country-col' className='mb-5 mb-xl-2' md={3}>
+      <Card id='mycards'  style={{border:"none"}}>
+        <Card.Img variant="top" src="../../images/ke.png" />
+        <Card.Body style={{backgroundColor:mode ? "hsl(0, 0%, 100%)" : "hsl(209, 23%, 22%)"}}>
+          <Card.Title style={{color: mode ? "black" : "white"}}>Card Title</Card.Title>
+          <Card.Text style={{color: mode ? "black" : "white"}}>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+        </Card>
+          
+        </Col>
+
+        <Col id='country-col' className='mb-5 mb-xl-2' md={3}>
+          
+        <Card id='mycards' style={{border:"none"}}>
+        <Card.Img variant="top" src="../../images/ke.png" />
+        <Card.Body style={{backgroundColor:mode ? "hsl(0, 0%, 100%)" : "hsl(209, 23%, 22%)"}}>
+          <Card.Title style={{color: mode ? "black" : "white"}}>Card Title</Card.Title>
+          <Card.Text style={{color: mode ? "black" : "white"}}>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+        </Card>
+          
+        </Col>
+
+        <Col id='country-col' className='mb-5 mb-xl-2' md={3}>
+          
+        <Card id='mycards' style={{border:"none"}}>
+        <Card.Img variant="top" src="../../images/ke.png" />
+        <Card.Body style={{backgroundColor:mode ? "hsl(0, 0%, 100%)" : "hsl(209, 23%, 22%)"}}>
+          <Card.Title style={{color: mode ? "black" : "white"}}>Card Title</Card.Title>
+          <Card.Text style={{color: mode ? "black" : "white"}}>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+        </Card>
+          
+        </Col>
+
+        <Col id='country-col' className='mb-5 mb-xl-2' md={3}>
+          
+        <Card id='mycards' style={{border:"none"}}>
+        <Card.Img variant="top" src="../../images/ke.png" />
+        <Card.Body style={{backgroundColor:mode ? "hsl(0, 0%, 100%)" : "hsl(209, 23%, 22%)"}}>
+          <Card.Title style={{color: mode ? "black" : "white"}}>Card Title</Card.Title>
+          <Card.Text style={{color: mode ? "black" : "white"}}>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+        </Card>
+          
+        </Col>
+
+      </Row>
+
+      <Row className='myrow1 '>
+
+        <Col id='country-col' className='mb-5 mb-xl-2' md={3}>
+
+        <Card id='mycards' style={{border:"none"}}>
+        <Card.Img variant="top" src="../../images/ke.png" />
+        <Card.Body style={{backgroundColor:mode ? "hsl(0, 0%, 100%)" : "hsl(209, 23%, 22%)"}}>
+          <Card.Title style={{color: mode ? "black" : "white"}}>Card Title</Card.Title>
+          <Card.Text style={{color: mode ? "black" : "white"}}>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+        </Card>
+          
+        </Col>
+
+        <Col id='country-col' className='mb-5 mb-xl-2' md={3}>
+          
+        <Card id='mycards' style={{border:"none"}}>
+        <Card.Img variant="top" src="../../images/ke.png" />
+        <Card.Body style={{backgroundColor:mode ? "hsl(0, 0%, 100%)" : "hsl(209, 23%, 22%)"}}>
+          <Card.Title style={{color: mode ? "black" : "white"}}>Card Title</Card.Title>
+          <Card.Text style={{color: mode ? "black" : "white"}}>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+        </Card>
+          
+        </Col>
+
+        <Col id='country-col' className='mb-5 mb-xl-2' md={3}>
+          
+        <Card id='mycards' style={{border:"none"}}>
+        <Card.Img variant="top" src="../../images/ke.png" />
+        <Card.Body style={{backgroundColor:mode ? "hsl(0, 0%, 100%)" : "hsl(209, 23%, 22%)"}}>
+          <Card.Title style={{color: mode ? "black" : "white"}}>Card Title</Card.Title>
+          <Card.Text style={{color: mode ? "black" : "white"}}>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+        </Card>
+          
+        </Col>
+
+        <Col id='country-col' className='mb-5 mb-xl-2' md={3}>
+          
+        <Card id='mycards' style={{border:"none"}}>
+        <Card.Img variant="top" src="../../images/ke.png" />
+        <Card.Body style={{backgroundColor:mode ? "hsl(0, 0%, 100%)" : "hsl(209, 23%, 22%)"}}>
+          <Card.Title style={{color: mode ? "black" : "white"}}>Card Title</Card.Title>
+          <Card.Text style={{color: mode ? "black" : "white"}}>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+        </Card>
+          
+        </Col>
+
+      </Row>
+
+    </Container>
     </>
   )
 }
