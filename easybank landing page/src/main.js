90d0d04e9d7efdcc7ed6dd1894c49c0d2aede81b
MuchiriAndrew <kariukia225@gmail.com>
2023-07-07 16:30:10 +0300

    update

--- easybank landing page/src/main.js
+++ easybank landing page/src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 
-
 createApp(App).use(router).mount('#app')
 import 'bootstrap/dist/js/bootstrap.js'
 
